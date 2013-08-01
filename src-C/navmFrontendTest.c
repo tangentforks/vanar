6b6174df5035a03ecd8411ccc4fd24cab3b97cf8
Mat2 <matasan@gmx.de>
2013-08-01 21:09:38 +0200

    added navmFrontendTest.c

---
+++ src-C/navmFrontendTest.c
@@ -0,0 +1,23 @@
+#include "navmConfig.h"
+
+int main (void)
+  {pNavmBackend test = navmInitBackendHandler (0xFFFF);
+     test->fDisAsm = true;
+
+     printf ("start\n");
+     
+     uOpcode testOpcode;
+       testOpcode.aOpcodes[0] = 0;   // liLI
+       testOpcode.aOpcodes[1] = 48;  // liADD
+       testOpcode.aOpcodes[2] = 216; // MUL
+       testOpcode.aOpcodes[3] = 96;  // liSHL       
+       testOpcode.aOpcodes[4] = 209; // BR
+     
+     sWord testImm[4]; pImm = (pWord) testImm;
+       pImm[0] = 0x101; pImm[1] = 0x202; pImm[2] = 0x309; pImm[3] = 0x04;
+            
+     navmFrontendDynScalRegShed (test, testOpcode);
+     //navmFrontendDynScal        (test, testOpcode);
+       
+   navmFreeBackendHandler (test);
+   return 0;}
